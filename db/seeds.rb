cd798895a34e51ffbff676bd9d5a6b5780c46857
fjasonvasquez <fjvasquez@aol.com>
2016-10-20 23:58:47 -0700

    Added phone attributes to User and made it required

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,26 @@
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow" )
+@user = User.create(email: "test@test.com", 
+										password: "asdfasdf", 
+										password_confirmation: "asdfasdf", 
+										first_name: "Jon", 
+										last_name: "Snow",
+										phone: "4247897079")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name" )
+AdminUser.create(email: "admin@test.com", 
+								 password: "asdfasdf", 
+								 password_confirmation: "asdfasdf", 
+								 first_name: "Admin", 
+								 last_name: "Name",
+								 phone: "4247897079")
 
 pust "1 Admin User create"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, 
+							rationale: "#{post} rationale content", 
+							user_id: @user.id, 
+							overtime_request: 2.5)
 end
 
 puts "100 posts have been created"

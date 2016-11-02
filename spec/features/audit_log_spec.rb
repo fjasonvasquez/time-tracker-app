1620cf305120a9207ef69c3024c2f65ea8a0fbca
fjasonvasquez <fjvasquez@aol.com>
2016-11-01 23:55:41 -0700

    Initial Audit Log

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'AuditLog Feature' do 
+	let(:audit_log) { FactoryGirl.create(:audit_log) }
+
+	describe 'index' do 
+		before do 
+			admin_user = FactoryGirl.create(:admin_useradmin_user)
+			login_as(admin_user, :scope => :admin_user) 
+		end
+
+		it 'has an index page that can be reached' do 
+			visit audit_logs_path
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'renders audit log content' do 
+			visit audit_logs_path
+			expect(page).to have_content(/SNOW, JON/)
+		end
+
+		xit 'cannot be accessed by non admin users'
+
+		end
+	end
+end

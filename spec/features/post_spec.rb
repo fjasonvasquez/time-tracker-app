03f9e1fe855bd455056ccf60e10bb1606bcfa76c
fjasonvasquez <fjvasquez@aol.com>
2016-12-13 00:41:29 -0800

    Completed self ref model for user manager/employee feature #2

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -43,11 +43,11 @@ describe 'navigate' do
 
 	describe 'new' do 
 		it 'has a link from the homepage' do
-			employee = Employee.create(first_name: 'Employee', last_name: 'Authorized', email: "employee@example.com", password: "asdfasdf", password_confirmation: "asdfasdf", phone: "5555555555")	
+			employee = Employee.create(first_name: 'Employee', last_name: 'Authorized', email: 'employee@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', phone: '5555555555')	
 			login_as(user, :scope => :user)
 			visit root_path
 
-			click_link("new_post_from_nav")
+			click_link('new_post_from_nav')
 			expect(page.status_code).to eq(200)
 		end
 	end
